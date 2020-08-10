943d41721503a8ca48572dac307f3b026831927f
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-08-10 17:07:34 +0200

    main lang fr files

---
+++ locales/fr/main.lang.php
@@ -0,0 +1,78 @@
+<?php
+// Language: Français 
+// Module: dcLatestVersionsLight - 0.1
+// Date: 2020-08-10 14:55:54 
+// Translated with dcTranslater - 2018.10.26 
+
+#_admin.php:35
+$GLOBALS['__l10n']['dcLatestVersionsLight'] = 'Plugin dernières versions de Doclear';
+
+#_admin.php:35
+$GLOBALS['__l10n']['Show the latest available versions of Dotclear in dashboard'] = 'Affiche les dernières versions de Dotclear sur le tableau de bord';
+
+#_admin.php:76
+$GLOBALS['__l10n']['Dotclear latests versions lite'] = 'Plugin dcLatestVersions, version "légère"';
+
+#_admin.php:103
+$GLOBALS['__l10n']['Show Dotclear latests versions'] = 'Montrer les dernières versions de Dotclear';
+
+#_admin.php:107
+$GLOBALS['__l10n']['Show Dotclear\'s latest versions on dashboard.'] = 'Montrer les dernières versions de Dotclear sur le tableau de bord';
+
+#_admin.php:167
+$GLOBALS['__l10n']['<li><a href="%u" title="Download Dotclear %v">%r</a> : %v</li>'] = '<li><a href="%u" title="Télécharger Dotclear %v">%r</a> : %v</li>';
+
+#_admin.php:208
+#debug.php:104
+#debug.php:114
+$GLOBALS['__l10n']['Dotclear\'s latest versions'] = 'Dernières versions de Dotclear';
+
+#debug.php:39
+$GLOBALS['__l10n']['<li><a href="%u" title="Download Dotclear version %v">%r</a> : %v</li>'] = '<li><a href="%u" title="Télécharger Dotclear %v">%r</a> : %v</li>';
+
+#debug.php:44
+$GLOBALS['__l10n']['exists'] = 'existe';
+
+#debug.php:44
+$GLOBALS['__l10n']['dont exists!'] = 'n\'existe pas !';
+
+#debug.php:48
+$GLOBALS['__l10n']['your cache versions dir %s dont exists ! Test %s.'] = 'Le  répertoire cache des versions %s n\'existe pas ! Essayer %s.';
+
+#debug.php:48
+#debug.php:60
+$GLOBALS['__l10n']['force dotclear update'] = 'de  forcer la mise à jour de Dotclear';
+
+#debug.php:53
+$GLOBALS['__l10n']['as files'] = 'contient des fichiers';
+
+#debug.php:53
+$GLOBALS['__l10n']['as no file!!'] = 'ne contient aucun fichier!!';
+
+#debug.php:58
+$GLOBALS['__l10n']['cache versions content'] = 'Contenu du cache versions';
+
+#debug.php:60
+$GLOBALS['__l10n']['There is  no file in your cache versions dir %s. Test %s.'] = 'Il n\'y a aucun fichier dans le cache versions dir %s. Test %s.';
+
+#debug.php:108
+$GLOBALS['__l10n']['what should be displayed'] = 'Ce qui devrait être affiché';
+
+#debug.php:140
+$GLOBALS['__l10n']['from'] = 'de';
+
+#index.php:57
+$GLOBALS['__l10n']['show on dashboard.'] = 'affiché sur le tableau de bord.';
+
+#index.php:57
+$GLOBALS['__l10n']['not display on dashboard.'] = 'pas montré sur le tableau de bord.';
+
+#index.php:82
+$GLOBALS['__l10n']['your current %s status is \'<i>%s</i>\''] = 'vos préférences actuelles %s sont \'<i>%s</i>\'';
+
+#index.php:86
+$GLOBALS['__l10n']['You can change this options in your %s %s.'] = 'Vous pouve modifier ces options dans %s %s.';
+
+#index.php:90
+$GLOBALS['__l10n']['also see below.'] = 'voir aussi plus bas';
+
