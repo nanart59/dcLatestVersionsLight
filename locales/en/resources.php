a0d7a0ed1299cb8aab1b102f78f92fcf5e8fc623
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-09-03 19:27:34 +0200

    help resources

---
+++ locales/en/resources.php
@@ -0,0 +1,23 @@
+<?php
+# -- BEGIN LICENSE BLOCK ----------------------------------
+#
+# This file is part of dcLatestVersionsLight, a plugin for Dotclear 2.
+# 
+# Copyright (c) 2020 Nan'Art and contributors
+# a light corrected and updated version of dcLatestVersions 2015-03-11, Jean-Christian Denis and contributors
+# 
+# Licensed under the GPL version 2.0 license.
+# A copy of this license is available in LICENSE file or at
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+# -- END LICENSE BLOCK ------------------------------------
+/**
+ * @brief dcLatestVersionsLight, a plugin for Dotclear 2
+ * 
+ * help resources loader
+ */
+/* dcLatestVersionsLight/locales/en/resources.php */
+
+if (!isset($__resources['help']['dcLatestVersionsLight'])) {
+    $__resources['help']['dcLatestVersionsLight'] = dirname(__FILE__) . '/help/help.html';
+}
