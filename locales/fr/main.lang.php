5e5a1f8daa0c3c0fd0695c5604cf6301ef7323cf
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-10-02 21:40:33 +0200

    Update main.lang.php

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,66 +1,59 @@
 <?php
 // Language: Français 
 // Module: dcLatestVersionsLight - 1.13
-// Date: 2020-10-02 23:11:59 
+// Date: 2020-10-02 19:21:17 
 // Translated with dcTranslater - 2018.10.26 
 
-#_admin.php:35
-$GLOBALS['__l10n']['dcLatestVersionsLight'] = 'Dotclear versions - light';
+#_admin.php:34
+$GLOBALS['__l10n']['dcLatestVersionsLight'] = 'Dotclear dernières versions Light';
 
-#_admin.php:35
+#_admin.php:34
 $GLOBALS['__l10n']['Show the latest available versions of Dotclear in dashboard'] = 'Montrer les dernières versions sur le tableau de bord';
 
-#_admin.php:76
-$GLOBALS['__l10n']['Dotclear latests versions light'] = 'Dotclear versions - light';
+#_admin.php:74
+$GLOBALS['__l10n']['Dotclear latests versions light'] = 'Dotclear dernières versions Light';
 
-#_admin.php:107
-$GLOBALS['__l10n']['Dotclear latests versions  - light'] = 'Dotclear versions - light';
-
-#_admin.php:112
+#_admin.php:116
 $GLOBALS['__l10n']['Show Dotclear\'s latest versions on dashboard.'] = 'Montrer les dernières versions sur le tableau de bord';
 
-#_admin.php:172
-$GLOBALS['__l10n']['<li><a href="%u" title="Download Dotclear %v">%r</a> : %v</li>'] = '<li><a href="%u" title="télécharger la version %v">%r</a> : %v</li>';
+#_admin.php:194
+$GLOBALS['__l10n']['Download version'] = 'Télécharger la version';
 
-#_admin.php:220
-$GLOBALS['__l10n']['Dotclear latests versions - light'] = 'Dotclear versions - light';
+#_admin.php:214
+$GLOBALS['__l10n']['Dotclear latests versions - light'] = 'Dotclear dernières versions Light';
 
-#debug.php:39
-$GLOBALS['__l10n']['<li><a href="%u" title="Download Dotclear version %v">%r</a> : %v</li>'] = '<li><a href="%u" title="Télécharger la version %v">%r</a> : %v</li>';
-
-#debug.php:44
+#debug.php:45
 $GLOBALS['__l10n']['exists'] = 'existe';
 
-#debug.php:44
+#debug.php:45
 $GLOBALS['__l10n']['dont exists!'] = 'n\'existe pas';
 
-#debug.php:48
+#debug.php:49
 $GLOBALS['__l10n']['your cache versions dir %s dont exists ! Test %s.'] = 'Le répertoire cache des versions %s n\'existe pas ! Essayer de %s';
 
-#debug.php:48
-#debug.php:60
+#debug.php:49
+#debug.php:61
 $GLOBALS['__l10n']['force dotclear update'] = 'forcer les mises à jour de Dotclear';
 
-#debug.php:53
+#debug.php:54
 $GLOBALS['__l10n']['as files'] = 'contient des fichiers';
 
-#debug.php:53
+#debug.php:54
 $GLOBALS['__l10n']['as no file!!'] = 'ne contient aucun fichier';
 
-#debug.php:58
+#debug.php:59
 $GLOBALS['__l10n']['cache versions content'] = 'contenu du cache versions';
 
-#debug.php:60
+#debug.php:61
 $GLOBALS['__l10n']['There is no file in your cache versions dir %s. Test %s.'] = 'Il n\'y a aucun fichier dans le répertoire cache versions %s. Essayer de %s';
 
-#debug.php:104
-#debug.php:114
+#debug.php:103
 $GLOBALS['__l10n']['Dotclear\'s latest versions'] = 'Dernières versions Dotclear';
 
-#debug.php:108
+#debug.php:107
 $GLOBALS['__l10n']['what should be displayed'] = 'ce qui devrait être affiché';
 
-#debug.php:140
+#debug.php:129
 $GLOBALS['__l10n']['from'] = 'de';
 
 #index.php:51
@@ -75,18 +68,20 @@ $GLOBALS['__l10n']['show on dashboard.'] = 'montrer sur le tableau de bord';
 #index.php:57
 $GLOBALS['__l10n']['not display on dashboard.'] = 'ne pas afficher sur le tableau de bord';
 
-#index.php:82
+#index.php:90
 $GLOBALS['__l10n']['your current %s status is \'<i>%s</i>\''] = 'Vos %s sont \'<i>%s</i>\'';
 
-#index.php:82
+#index.php:90
 $GLOBALS['__l10n']['preferences about dotclear updates'] = 'préférences à propos des mises à jour de Dotclear';
 
-#index.php:86
+#index.php:96
 $GLOBALS['__l10n']['You can change this options in your %s %s.'] = 'Vous pouvez modifier ces options dans vos %s %s.';
 
-#index.php:89
+#index.php:99
 $GLOBALS['__l10n']['Preferences on Dashboard'] = 'Préférences sur le tableau de bord';
 
-#index.php:90
+#index.php:100
 $GLOBALS['__l10n']['also see below.'] = 'voir aussi ci-dessous';
+
+$GLOBALS['__l10n']['Dotclear latests versions  - light'] = 'Dotclear dernières versions Light';
 
