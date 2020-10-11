d6e9eb187a9781d322881d9be74e56a842ead145
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-10-11 19:04:32 +0200

    Update main.lang.php

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,87 +1,129 @@
-<?php
-// Language: Français 
-// Module: dcLatestVersionsLight - 1.13
-// Date: 2020-10-02 19:21:17 
-// Translated with dcTranslater - 2018.10.26 
+# Language: Français
+# Module: dcLatestVersionsLight - 1.13
+# Date: 2020-10-11 16:24:56
+# Translated with translater 2018.10.26
 
-#_admin.php:34
-$GLOBALS['__l10n']['dcLatestVersionsLight'] = 'Dotclear dernières versions Light';
+msgid ""
+msgstr ""
+"Content-Type: text/plain; charset=UTF-8\n"
+"Project-Id-Version: dcLatestVersionsLight 1.13\n"
+"POT-Creation-Date: \n"
+"PO-Revision-Date: 2020-10-11T16:24:56+00:00\n"
+"Last-Translator: nanart\n"
+"Language-Team: \n"
+"MIME-Version: 1.0\n"
+"Content-Transfer-Encoding: 8bit\n"
+"Plural-Forms: nplurals=2; plural=(n > 1);\n"
 
-#_admin.php:34
-$GLOBALS['__l10n']['Show the latest available versions of Dotclear in dashboard'] = 'Montrer les dernières versions sur le tableau de bord';
+#: _admin.php:34
+msgid "dcLatestVersionsLight"
+msgstr "Dotclear dernières versions Light"
 
-#_admin.php:74
-$GLOBALS['__l10n']['Dotclear latests versions light'] = 'Dotclear dernières versions Light';
+#: _admin.php:34
+msgid "Show the latest available versions of Dotclear in dashboard"
+msgstr "Montrer les dernières versions sur le tableau de bord"
 
-#_admin.php:116
-$GLOBALS['__l10n']['Show Dotclear\'s latest versions on dashboard.'] = 'Montrer les dernières versions sur le tableau de bord';
+#: _admin.php:74
+msgid "Dotclear latests versions light"
+msgstr "Dotclear dernières versions Light"
 
-#_admin.php:194
-$GLOBALS['__l10n']['Download version'] = 'Télécharger la version';
+#: _admin.php:116
+msgid "Show Dotclear's latest versions on dashboard."
+msgstr "Montrer les dernières versions sur le tableau de bord"
 
-#_admin.php:214
-$GLOBALS['__l10n']['Dotclear latests versions - light'] = 'Dotclear dernières versions Light';
+#: _admin.php:195
+msgid "Download version"
+msgstr "Télécharger la version"
 
-#debug.php:45
-$GLOBALS['__l10n']['exists'] = 'existe';
+#: _admin.php:215
+msgid "Dotclear latests versions - light"
+msgstr "Dotclear dernières versions Light"
 
-#debug.php:45
-$GLOBALS['__l10n']['dont exists!'] = 'n\'existe pas';
+#: debug.php:45
+msgid "exists"
+msgstr "existe"
 
-#debug.php:49
-$GLOBALS['__l10n']['your cache versions dir %s dont exists ! Test %s.'] = 'Le répertoire cache des versions %s n\'existe pas ! Essayer de %s';
+#: debug.php:45
+msgid "dont exists!"
+msgstr "n'existe pas"
 
-#debug.php:49
-#debug.php:61
-$GLOBALS['__l10n']['force dotclear update'] = 'forcer les mises à jour de Dotclear';
+#: debug.php:49
+msgid "your cache versions dir %s dont exists ! Test %s."
+msgstr "Le répertoire cache des versions %s n'existe pas ! Essayer de %s"
 
-#debug.php:54
-$GLOBALS['__l10n']['as files'] = 'contient des fichiers';
+#: debug.php:49
+#: debug.php:61
+msgid "force dotclear update"
+msgstr "forcer les mises à jour de Dotclear"
 
-#debug.php:54
-$GLOBALS['__l10n']['as no file!!'] = 'ne contient aucun fichier';
+#: debug.php:54
+msgid "as files"
+msgstr "contient des fichiers"
 
-#debug.php:59
-$GLOBALS['__l10n']['cache versions content'] = 'contenu du cache versions';
+#: debug.php:54
+msgid "as no file!!"
+msgstr "ne contient aucun fichier"
 
-#debug.php:61
-$GLOBALS['__l10n']['There is no file in your cache versions dir %s. Test %s.'] = 'Il n\'y a aucun fichier dans le répertoire cache versions %s. Essayer de %s';
+#: debug.php:59
+msgid "cache versions content"
+msgstr "contenu du cache versions"
 
-#debug.php:103
-$GLOBALS['__l10n']['Dotclear\'s latest versions'] = 'Dernières versions Dotclear';
+#: debug.php:61
+msgid "There is no file in your cache versions dir %s. Test %s."
+msgstr "Il n'y a aucun fichier dans le répertoire cache versions %s. Essayer de %s"
 
-#debug.php:107
-$GLOBALS['__l10n']['what should be displayed'] = 'ce qui devrait être affiché';
+#: debug.php:103
+msgid "Dotclear's latest versions"
+msgstr "Dernières versions Dotclear"
 
-#debug.php:129
-$GLOBALS['__l10n']['from'] = 'de';
+#: debug.php:107
+msgid "what should be nearly displayed on dashboard"
+msgstr "Ce qui devrait approximativement être affiché sur le tableau de bord"
 
-#index.php:51
-$GLOBALS['__l10n']['Plugin informations'] = 'Informations plugin';
+#: debug.php:129
+msgid "from"
+msgstr "de"
 
-#index.php:52
-$GLOBALS['__l10n']['plugin %s information'] = 'informationsplugin %s';
+#: index.php:54
+msgid "Plugin informations"
+msgstr "Informations plugin"
 
-#index.php:57
-$GLOBALS['__l10n']['show on dashboard.'] = 'montrer sur le tableau de bord';
+#: index.php:55
+msgid "plugin %s information"
+msgstr "informationsplugin %s"
 
-#index.php:57
-$GLOBALS['__l10n']['not display on dashboard.'] = 'ne pas afficher sur le tableau de bord';
+#: index.php:60
+msgid "show on dashboard."
+msgstr "montrer sur le tableau de bord"
 
-#index.php:90
-$GLOBALS['__l10n']['your current %s status is \'<i>%s</i>\''] = 'Vos %s sont \'<i>%s</i>\'';
+#: index.php:60
+msgid "not display on dashboard."
+msgstr "ne pas afficher sur le tableau de bord"
 
-#index.php:90
-$GLOBALS['__l10n']['preferences about dotclear updates'] = 'préférences à propos des mises à jour de Dotclear';
+#: index.php:93
+msgid "your current %s status is '<strong>%s</strong>'"
+msgstr "Votre statut actuel %s est '<strong>%s</strong>'"
 
-#index.php:96
-$GLOBALS['__l10n']['You can change this options in your %s %s.'] = 'Vous pouvez modifier ces options dans vos %s %s.';
+#: index.php:93
+msgid "preferences about dotclear updates"
+msgstr "préférences à propos des mises à jour de Dotclear"
 
-#index.php:99
-$GLOBALS['__l10n']['Preferences on Dashboard'] = 'Préférences sur le tableau de bord';
+#: index.php:99
+msgid "You can change this options in your %s %s."
+msgstr "Vous pouvez modifier ces options dans vos %s %s."
 
-#index.php:100
-$GLOBALS['__l10n']['also see below.'] = 'voir aussi ci-dessous';
+#: index.php:102
+msgid "Preferences on Dashboard"
+msgstr "Préférences sur le tableau de bord"
 
-$GLOBALS['__l10n']['Dotclear latests versions  - light'] = 'Dotclear dernières versions Light';
+#: index.php:103
+msgid "also see below: "
+msgstr "voir aussi plus bas: "
 
+#: index.php:110
+msgid "Your Dotclear version is %s - %s."
+msgstr "Votre version Dotclear :  %s - %s."
+
+#: index.php:112
+msgid "Your php version is %s."
+msgstr "Votre version php : %s."
