6500017bf9cffceee7a338f57c20bb90ec57bd74
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-10-11 18:59:46 +0200

    Update _define.php

--- _define.php
+++ _define.php
@@ -11,23 +11,6 @@
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 #
 # -- END LICENSE BLOCK ------------------------------------
-/**
- * @brief dcLatestVersionsLight, a plugin for Dotclear 2
- * 
- * define plugin':
-	- name: dcLatestVersionsLight
-	- description: Show the latest available versions of Dotclear in dashboard
-	- author(s): Nan'Art and contributors
-	- version: 1.13
-	- properties
-		. require (tested) Dc 2.9.x or >
-		. require (tested)php 5.6.40 7.4.1
-		- usage: content admin -- no widget
-		- setting: 
-			user pref favorite, class dcLatestVersionsLight
-		- support: https://forum.dotclear.org/viewtopic.php?id=49826
-		- details: https://plugins.dotaddict.org/dc2/details/dcLatestVersionsLight
- */
 /* dcLatestVersionsLight/_define.php */
 
 if (!defined('DC_RC_PATH')) {
@@ -42,7 +25,7 @@ $this->registerModule(
 	/* Author */
 	"Nan'Art and contributors",
 	/* Version */
-	'1.13',
+	'1.14',
 	/* Properties */
 	[
 		'requires'    => [['core', '2.9']],				// dotclear min version (tested)
