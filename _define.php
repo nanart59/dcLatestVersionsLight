5582c5e66141b68385d0340fad654bc3b478d714
Nan'Art <62995223+nanart59@users.noreply.github.com>
2020-12-15 16:05:39 +0100

    Update _define.php

--- _define.php
+++ _define.php
@@ -25,7 +25,7 @@ $this->registerModule(
 	/* Author */
 	"Nan'Art and contributors",
 	/* Version */
-	'1.15',
+	'1.16',
 	/* Properties */
 	[
 		'requires'    => [['core', '2.9']],				// dotclear min version (tested)
